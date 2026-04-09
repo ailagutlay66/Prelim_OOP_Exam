1ed17b9801e67b2488fbaa19cc99b836f12070ec
ailagutlay66 <ailagutlay66@gmail.com>
2026-04-09 13:11:53 +0800

    Refactor pointer basics code structure

--- pointer basics_gutlay.cpp
+++ pointer basics_gutlay.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-    // A. Declare pointers
+    
     double *d_var;
     double *d_array;
 
-    // B. Dynamic allocation
+   
     d_var = new double;
     d_array = new double[10];
 
-    // C. Input value
+   
     cout << "Enter a value: ";
     cin >> *d_var;
     cout << "Value entered: " << *d_var << endl;
 
-    // D. Initialize array to 1.0
+    
     for(int i = 0; i < 10; i++) {
         d_array[i] = 1.0;
     }
@@ -25,7 +25,7 @@ int main() {
         cout << d_array[i] << endl;
     }
 
-    // E. Deallocate memory
+   
     delete d_var;
     delete[] d_array;
 
